257a082bdd17bbd1d5e88190dd2fd9b123552773
T42Mobile <info@think42labs.com>
2016-11-28 12:51:48 +0530

    Pappaya created

---
+++ Pappaya/Pappaya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pappaya
+//
+//  Created by Thirumal on 28/11/16.
+//  Copyright © 2016 Think42labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
